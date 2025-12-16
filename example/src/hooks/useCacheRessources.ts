95f8b59e3581b6daed7368a740811b0109481836
julien <julien.brayere@obitrain.com>
2025-12-16 22:14:38 +0100

    fix: small fixes

--- example/src/hooks/useCacheRessources.ts
+++ example/src/hooks/useCacheRessources.ts
@@ -1,6 +1,5 @@
 import { FontAwesome } from '@expo/vector-icons';
-import * as Font from
-'expo-font';
+import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect, useState } from 'react';
 
