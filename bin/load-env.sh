8fcd42b68027767872f67b2ed0f12c7852462b86
julien <julien.brayere@obitrain.com>
2025-12-16 22:20:45 +0100

    chore: fix load env

--- bin/load-env.sh
+++ bin/load-env.sh
@@ -2,21 +2,24 @@
 
 set -eou pipefail
 
-readonly _VAULT_NAME='Obitrain-prd'
-readonly _ENV_VAR_NAME='GITLAB_REGISTRY_TOKEN_DESIGN'
+readonly BWS_URL="https://vault.bitwarden.eu"
+
+readonly _VAULT_NAME=${VAULT_NAME:='Obitrain-prd'}
+readonly _ENV_NAME=${ENV_NAME:=".env"}
+readonly _GITLAB_TOKEN_NAME="GITLAB_REGISTRY_TOKEN_DESIGN"
+
 cd "$(dirname "$0")/.." || exit 1
 
-
-readonly PROJECT_ID=$(bws project list | jq --raw-output ".[] | select( .name == \"$_VAULT_NAME\") | .id")
+readonly PROJECT_ID=$(bws project list -u "$BWS_URL" | jq --raw-output ".[] | select( .name == \"$_VAULT_NAME\") | .id")
 if [ -z "$PROJECT_ID" ]
 then
     echo "Project $_VAULT_NAME not found"
     exit 1
 fi
 
-echo "Loading $_ENV_VAR_NAME from $_VAULT_NAME"
-bws secret list | jq -c -r ".[] | select( .projectId == \"$PROJECT_ID\" and .key == \"$_ENV_VAR_NAME\") | \"\\(.key)=\\(.value)\"" > .env
+echo "Loading $_GITLAB_TOKEN_NAME from $_VAULT_NAME"
+bws secret list -u "$BWS_URL" | jq -c -r ".[] | select( .projectId == \"$PROJECT_ID\" and .key == \"$_GITLAB_TOKEN_NAME\") | \"\\(.key)=\\(.value)\"" > "$_ENV_NAME"
 set -a  # automatically export all variables
-source .env
+source "$_ENV_NAME"
 set +a  # turn off automatic export
 
